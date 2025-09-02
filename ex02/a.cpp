7d7992d5b07768f9368aa017949b658c77acc0d6
tsi <timo42@proton.me>
2025-09-02 17:10:13 +0200

    Progress on a.cpp

--- ex02/a.cpp
+++ ex02/a.cpp
@@ -54,29 +54,111 @@ bool establish_main_chain(int ints[], std::size_t size, std::size_t chunk_size) 
     return true;
 }
 
-void binary_insert(int ints[], std::size_t size, std::size_t idx, std::size_t chunk_size) { int number_to_insert = get_biggest_in_chunk(ints, idx, chunk_size); }
-// problem: how do we do binary search/insertion when part of the array is half as dense as the other half?
+// does a normal binary insert on an array, however, after (and including) a threshold value, binary search should
+// only consider every other element.
+bool virtual_binary_insert(int ints[], std::size_t size, std::size_t idx, std::size_t chunk_size, std::size_t binary_insertion_threshold) {
+    if (idx + chunk_size - 1 >= size) {
+        // out of bounds, don't increase threshold
+        return false;
+    }
+    int number_to_insert = get_biggest_in_chunk(ints, idx, chunk_size);
+    std::cout << "Number to insert: " << number_to_insert << std::endl;
+    return true;
+}
+// Further Notes:
+//
+// Problem: how do we do binary search/insertion when part of the array is half as dense as the other half?
+//
+// main chain:    c1, c2, a2, a3, a4, a5, a6, a7, a8, a9, a10
+// pending chain:        b2, b3, b4, b5, b6, b7, b8, b9, b10, b11
+// [c1, c2, b2, a2, b3, a3, b4, a4, b5, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10, b11]
+//
+// let's insert b3
+// for that, we need to do binary search on [c1, c2, a2]. However, inside the array, we find
+// [c1, c2, b2, a2]. Therefore, we need a function that maps the virtual binary search indices to
+// the actual array indices. Something like
+// - `actual_idx = virtual_idx < threshold ? virtual_idx : threshold + 1 + (virtual_idx - threshold) * 2
+// In our example, threshold is 2 (assuming 0-indexing)
+// Going through all 3 possible values of virtual_idx, we can calculate actual_idx:
+// actual_idx = 0 < 2 ? 0 : 2 + 1 + (0 - 2) * 2 = 0 // normal idx
+// actual_idx = 1 < 2 ? 1 : 2 + 1 + (1 - 2) * 2 = 1 // normal idx
+// actual_idx = 2 < 2 ? 2 : 2 + 1 + (2 - 2) * 2 = 3 // threshold jump!
+// If we had a 4th element, our formula would go in steps of 2, which is correct.
+//
+// Let's derive a general method for determining the threshold value.
+//
+// Example run through (threshold in quotes):
+// [c1, c2, "b2", a2, b3, a3, b4, a4, b5, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10, b11] // after first recursive call
+// [c1, c2, c3, "b2", a2, a3, b4, a4, b5, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10, b11]
+// [c1, c2, c3, c4, c5, c6, "b4", a4, b5, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10, b11]
+// [c1, c2, c3, c4, c5, c6, c7, "b4", a4, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10, b11]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, "b6", a6, b7, a7, b8, a8, b9, a9, b10, a10, b11]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, "b6", a6, b7, a7, b8, a8, b9, a9, b10, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, "b6", a6, b7, a7, b8, a8, b9, a9, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, "b6", a6, b7, a7, b8, a8, a9, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, "b6", a6, b7, a7, a8, a9, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, "b6", a6, a7, a8, a9, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21] // sorted
+//
+// Example run through (threshold in quotes) with last jacobsthal index being one smaller:
+// [c1, c2, "b2", a2, b3, a3, b4, a4, b5, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10] // after first recursive call
+// [c1, c2, c3, "b2", a2, a3, b4, a4, b5, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10]
+// [c1, c2, c3, c4, c5, c6, "b4", a4, b5, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10]
+// [c1, c2, c3, c4, c5, c6, c7, "b4", a4, a5, b6, a6, b7, a7, b8, a8, b9, a9, b10, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, "b6", a6, b7, a7, b8, a8, b9, a9, b10, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, "b6", a6, b7, a7, b8, a8, b9, a9, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, "b6", a6, b7, a7, b8, a8, a9, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, "b6", a6, b7, a7, a8, a9, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, "b6", a6, a7, a8, a9, a10]
+// [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20] // sorted
+// Evidently, the threshold values are the same, and the iteration for the b11 is skipped (and the threshold value is not incremented
+// for that skipped iteration)
+//
+// both values 0-indexed
+// values of idx_pending_chain | threshold
+// 2                           | 2 (+1)
+// 1                           | 3 (+1)
+// 4                           | 6 (+3, new jacobsthal number)
+// 3                           | 7 (+1)
+// 10                          | 10 (+3, new jacobsthal number)
+// 9                           | 11 (+1)
+// 8                           | 12 (+1)
+// 7                           | 13 (+1)
+// 6                           | 14 (+1)
+// 5                           | 15 (+1)
+//
+// Evidently, in our binary insertion loop, we can trivially update the threshold value every time:
+// increase it by 1 every time, and by 2 every time a new jacobsthal number starts.
 
-void binary_insertion_downwards(int ints[], std::size_t size, std::size_t chunk_size, std::size_t jacobsthal, std::size_t prev_jacobsthal) {
+bool binary_insertion_downwards(int ints[], std::size_t size, std::size_t chunk_size, std::size_t jacobsthal, std::size_t prev_jacobsthal, std::size_t *binary_insertion_threshold) {
     --jacobsthal; // decrementing since `ints` is 0-indexed
     --prev_jacobsthal;
 
     if (jacobsthal >= size)
         jacobsthal = size - 1;
 
+    bool did_something = false;
     for (std::size_t jacobs_idx = jacobsthal; jacobs_idx > prev_jacobsthal; --jacobs_idx) {
         std::size_t actual_idx = jacobs_idx * chunk_size * 2;
-        binary_insert(ints, size, actual_idx, chunk_size);
+        if (virtual_binary_insert(ints, size, actual_idx, chunk_size, *binary_insertion_threshold)) {
+            did_something = true;
+            ++*binary_insertion_threshold;
+        }
     }
+    return did_something;
 }
 
-void binary_insertion_accordion_to_jacobsthal_numbering(int ints[], std::size_t size, std::size_t chunk_size) {
+void binary_insertion_according_to_jacobsthal_numbering(int ints[], std::size_t size, std::size_t chunk_size) {
     std::size_t prev_jacobsthal = 1;
     std::size_t jacobsthal = 3;
+    std::size_t binary_insertion_threshold = 2;
+
     while (true) {
-        binary_insertion_downwards(ints, size, chunk_size, jacobsthal, prev_jacobsthal);
+        if (!binary_insertion_downwards(ints, size, chunk_size, jacobsthal, prev_jacobsthal, &binary_insertion_threshold))
+            break;
         jacobsthal += prev_jacobsthal * 2;
         prev_jacobsthal = jacobsthal - prev_jacobsthal * 2;
+        binary_insertion_threshold += 2;
     }
 }
 
@@ -86,7 +168,7 @@ void sort_main_chain(int ints[], std::size_t size, std::size_t chunk_size) {
     std::cout << "This is the main chain with chunk size " << chunk_size << std::endl;
     print_array(ints, size, chunk_size);
     sort_main_chain(ints, size, chunk_size << 1);
-    binary_insertion_accordion_to_jacobsthal_numbering(ints, size, chunk_size);
+    binary_insertion_according_to_jacobsthal_numbering(ints, size, chunk_size);
 }
 
 void ford_johnson(int ints[], std::size_t size) {
